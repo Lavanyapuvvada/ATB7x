5b4cbdbe67cdb4a36d12bc4e120a9cba13456a79
Lavanya Puvvada <contactlavanypuvvada@gmail.com>
2024-07-21 01:31:24 +0530

    Triangle, leaap year and Fizzbuzz programs got added

---
+++ src/lab21.java
@@ -0,0 +1,14 @@
+public class lab21 {
+    public static void main(String[] args) {
+        int a=89;
+        int b=99;
+        if(a>b)
+        {
+            System.out.println(" a is large");
+        }
+        else
+        {
+            System.out.println(" b is larger");
+        }
+    }
+}
